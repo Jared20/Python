1cb1bbf0c76f7ddeb9c3b75cff188fe1e841a141
Jared20 <68714490+Jared20@users.noreply.github.com>
2021-03-11 08:33:53 -0600

    Add files via upload

---
+++ python.py
@@ -0,0 +1,86 @@
+#Manejo strings
+"""print("hola mundo")
+print("soy jared")"""
+
+"""print("Esto es una suma")
+numero_uno=int(input("ingrese un numero"))
+numero_dos=int(input("ingrese otro numero"))
+resultado=numero_uno+numero_dos
+print(resultado)"""
+
+'''mensaje = "hola"
+mensaje +=" "
+mensaje +="Jared"
+print(mensaje)
+print('concatenación:')
+#concatenación:
+#NOTA: python distingue concatenación(abajo)de suma(arriba arriba no arriba superior).
+mensaje = "hola"
+espacio =" "
+mensaje2 ="Jared"
+print(mensaje+espacio+mensaje2)'''
+
+'''mensaje = "hola"
+mensaje +=" "
+mensaje +="Jared"
+buscar_subcadena = mensaje.find("Jared")
+#.find busca la palabra y te dice su posición. 
+print(mensaje)
+print(buscar_subcadena)'''
+ 
+'''mensaje = "hola"
+mensaje +=" "
+mensaje +="Jared"
+buscar_subcadena2 = mensaje[1:8]
+#los numeros son las posiciones.
+print(mensaje)
+print(buscar_subcadena2)
+'''
+#Manejo strings
+#Operadores aritméticos en Python.
+'''print('suma:')
+num_1 = int(input('ingrese num 1:'))
+num_2 = int(input('ingrese num 2:'))
+resultado = num_1+num_2
+print('el resultado es:', resultado)
+'''
+'''print('resta:')
+num_1 = int(input('ingrese num 1:'))
+num_2 = int(input('ingrese num 2:'))
+resultado = num_1-num_2
+print('el resultado es:', resultado)
+'''
+'''print('multiplicación:')
+num_1 = int(input('ingrese num 1:'))
+num_2 = int(input('ingrese num 2:'))
+resultado = num_1*num_2
+print('el resultado es:', resultado)'''
+
+'''print('potenciación:')
+num_1 = int(input('ingrese num 1:'))
+exponente = int(input('ingrese exponente:'))
+resultado = num_1**num_2
+print('el resultado es:', resultado)
+'''
+'''print('divición:')
+num_1 = int(input('ingrese num 1:'))
+num_2 = int(input('ingrese num 2:'))
+resultado = num_1/num_2
+print('el resultado es:', resultado)'''
+
+'''print('módulo:')
+num_1 = int(input('ingrese num 1:'))
+num_2 = int(input('ingrese num 2:'))
+resultado = num_1%num_2
+print('el resultado es:', resultado)
+'''
+'''print('divición entera:')
+num_1 = int(input('ingrese num 1:'))
+num_2 = int(input('ingrese num 2:'))
+resultado = num_1//num_2
+print('el resultado es:', resultado)
+'''
+#Operadores aritméticos en Python.
+#Entrada de datos desde el teclado.
+
+#Entrada de datos desde el teclado.
